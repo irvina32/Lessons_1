d2d3b0ce295001b9c4d673434b1cfc727af8ba45
Irina <irvina@inbox.ru>
2022-10-17 17:19:03 +0300

    Внесли дополнения на варианты имени

--- REPO/Examples/Ex_006_Condition/Program.cs
+++ REPO/Examples/Ex_006_Condition/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.Write("Введите имя пользователя: ");
 string username = Console.ReadLine();
-if (username == "Маша")
+if (username.ToLower == "Маша")
 {
     Console.WriteLine("Ура,это же Маша");
 }
