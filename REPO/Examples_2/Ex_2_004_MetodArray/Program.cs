636a285d7b0e36f0b23feea38fd7eef3d049f880
Irina <irvina@inbox.ru>
2022-10-18 15:40:18 +0300

    .\Program.cs

---
+++ REPO/Examples_2/Ex_2_004_MetodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int [] array = {1, 12, 23, 25, 34, 114, 13, 78 };
+
+int n = array.Length;
+int find = 5;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    // index = index + 1;
+    index++;
+}
